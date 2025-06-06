9167bf4b08c624b82fd8cf5b3a7776369fab23fd
Fred Morrison <frederick.morrison@outlook.com>
2025-06-06 14:22:49 -0600

    verify basic MongoDB connectivity

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 from importlib.metadata import version
 
+#
+from pymongo import MongoClient
 
+#
 from logging_utility import LoggingUtility
 from program_settings import ProgramSettings
 
 logger = LoggingUtility.start_logging()
+
 
 def get_python_version() -> str:
     return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
@@ -14,8 +18,51 @@ def get_python_version() -> str:
 def get_package_version(package_name: str) -> str:
     return version(package_name)
 
+
+def get_connection_string() -> str:
+    """
+    Get a connection string for MongoDB using the key/values stored in the .env file.
+    :return: a string containing the connection string.
+    """
+    template: str = ProgramSettings.get_setting('MONGODB_CONNECTION_TEMPLATE')
+    uid: str = ProgramSettings.get_setting('MONGODB_UID')
+    pwd: str = ProgramSettings.get_setting('MONGODB_PWD')
+
+    conn_string = f'mongodb+srv://{uid}:{pwd}@{template}'
+
+    msg = f'{conn_string=}'
+    logger.info(msg)
+    logger.debug(msg)
+
+    return conn_string
+
+
+def get_mongodb_client() -> MongoClient:
+    """
+    Get a MongoClient instance using the connection string
+    :return: MongoClient
+    """
+    return MongoClient(get_connection_string())
+
+
+def display_available_databases() -> None:
+    """
+    Display available databases.
+    """
+    # Connect to MongoDB
+    client: MongoClient = get_mongodb_client()
+
+    databases: list[str] = client.list_database_names()
+    msg = "Available Databases:"
+    LoggingUtility.log_info_and_debug(msg)
+    for database in databases:
+        msg = f'\t{database}'
+        LoggingUtility.log_info_and_debug(msg)
+
+
 def main():
-    pass
+    display_available_databases()
+
 
 if __name__ == '__main__':
     msg = f'Python version: {get_python_version()}'
@@ -27,4 +74,3 @@ if __name__ == '__main__':
     logger.debug(msg)
 
     main()
-
